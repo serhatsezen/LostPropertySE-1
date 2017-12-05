935c211296772913c96730a9ff71cbfe6fdf7a47
serhatsezn <serhatsezen@outlook.com.tr>
2017-12-05 12:16:34 +0300

    EditProfile Map View

--- app/src/main/java/com/team3s/lostpropertyse/Profile/EditProfileFragment.java
+++ app/src/main/java/com/team3s/lostpropertyse/Profile/EditProfileFragment.java
@@ -1,8 +1,11 @@
 package com.team3s.lostpropertyse.Profile;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,9 +16,14 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -38,12 +46,13 @@ public class EditProfileFragment extends Fragment {
 
     private FirebaseUser currentUser;
     private DatabaseReference curentUserRef;
-
+    double latMarkers;
+    double lngMarkers;
+    public LatLng user;
 
     public EditProfileFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -58,6 +67,7 @@ public class EditProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         Uid = currentUser.getUid();
         profileİmg = (ImageView) v.findViewById(R.id.EditPP);
@@ -66,6 +76,63 @@ public class EditProfileFragment extends Fragment {
         AdSoyad = (EditText) v.findViewById(R.id.editTextAdSoyad);
         Sehir = (EditText) v.findViewById(R.id.editTextSehir);
         mapView = (MapView) v.findViewById(R.id.mapView);
+
+
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume(); // needed to get the map to display immediately
+
+
+        curentUserRef.child("latLng").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                latMarkers = (double) snapshot.child("latitude").getValue();
+                lngMarkers = (double) snapshot.child("longitude").getValue();
+                user = new LatLng(latMarkers, lngMarkers);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                myGoogleMap = mMap;
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                myGoogleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                myGoogleMap.addMarker(new MarkerOptions().position(user).title("Bulundugunuz yer").snippet(""));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(user).zoom(12).build();
+                myGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+
+
 
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
